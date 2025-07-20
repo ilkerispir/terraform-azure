22608747aa3a4221f887fefaa0bced7437cc95a1
Ilker Ispir <ilker@ispir.dev>
2025-07-20 11:02:46 +1000

    fix: modified helm provider block

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -21,4 +21,11 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
 }
 
-provider "helm" {}
+provider "helm" {
+  kubernetes {
+    host                   = module.aks.kube_config[0].host
+    client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(module.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
+  }
+}

6efbce2bc4911ada5c7a3c7f1abeae413be249a3
Ilker Ispir <ilker@ispir.dev>
2025-07-20 14:16:58 +1000

    fix: modified aks output variables

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -15,17 +15,17 @@ terraform {
 }
 
 provider "kubernetes" {
-  host                   = module.aks.kube_config[0].host
-  client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(module.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
+  host                   = module.aks.host
+  client_certificate     = base64decode(module.aks.client_certificate)
+  client_key             = base64decode(module.aks.client_key)
+  cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = module.aks.kube_config[0].host
-    client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(module.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
+    host                   = module.aks.host
+    client_certificate     = base64decode(module.aks.client_certificate)
+    client_key             = base64decode(module.aks.client_key)
+    cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
   }
 }
--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -3,7 +3,22 @@ output "aks_id" {
   value       = azurerm_kubernetes_cluster.this.id
 }
 
-output "kube_config" {
-  description = "The kubeconfig for the AKS cluster."
-  value       = azurerm_kubernetes_cluster.this.kube_config_raw
+output "host" {
+  value     = azurerm_kubernetes_cluster.this.kube_config[0].host
+  sensitive = true
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.this.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value     = azurerm_kubernetes_cluster.this.kube_config[0].client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+  sensitive = true
 }
